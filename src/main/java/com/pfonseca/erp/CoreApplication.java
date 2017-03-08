d5bd2ac256da8893e55ddbce5b0f118cedffc105
pfonseca <pfonseca@atech.com.br>
2017-03-07 21:21:15 -0300

    Oauth2 authentication

--- src/main/java/com/pfonseca/erp/CoreApplication.java
+++ src/main/java/com/pfonseca/erp/CoreApplication.java
@@ -3,9 +3,11 @@ package com.pfonseca.erp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableResourceServer
 public class CoreApplication {
 
 	public static void main(String[] args) {
